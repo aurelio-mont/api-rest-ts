e3d473791e09e477f4be615765b68af1810ebce2
aurelio-mont <aurelio.mon@me.comt>
2024-04-09 20:55:06 -0600

    build: :rocket: Implent MongDB

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import { router } from "./routes";
+import db from "./config/mongo"
+const PORT = process.env.PORT || 3001;
+const app = express();
+app.use(cors());
+db().then(() => console.log("Coneccion a la BD lista"))
+app.use(router);
+app.listen(PORT, () => console.log(`Listo en el puerto ${PORT}`));
